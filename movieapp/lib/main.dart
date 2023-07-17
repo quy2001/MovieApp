a75403e3352df42078863f5895b9bfb6e6146ae3
TranQuy <62061964+quy2001@users.noreply.github.com>
2023-07-18 01:13:16 +0700

    call api

--- movieapp/lib/main.dart
+++ movieapp/lib/main.dart
@@ -2,14 +2,9 @@ import 'dart:async';
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movieapp/data/core/api_client.dart';
-import 'package:movieapp/data/data_sources/movie_remote_data_source.dart';
-import 'package:movieapp/data/repositories/movie_repository_impl.dart';
 import 'package:movieapp/domain/entities/app_error.dart';
 import 'package:movieapp/domain/entities/movie_entity.dart';
 import 'package:movieapp/domain/entities/no_params.dart';
-import 'package:movieapp/domain/repositories/movie_reponsitory.dart';
 import 'package:movieapp/domain/usecases/get_trending.dart';
 import 'package:movieapp/di/get_it.dart' as getIt;
 
